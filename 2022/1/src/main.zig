54289344b895319eebed993a4c1defe91355eba7
Alexander Vershilov <alexander.vershilov@sirius.online>
2022-12-07 11:38:27 +0600

    Day7: pt1+pt2 haskell only

--- 2022/1/src/main.zig
+++ 2022/1/src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
+pub fn groups(std.Iterator(a), a -> bool) std.Iterator(std.Iterator(a), a-> bool){
+}
+
+pub fn iterateBlocks(reader: std.io.Reader) !void {
+    var x = fn() -> std.Iterator([] const u8) {
+       var buf = try reader.readLine();
+       while (not std.mem.eql(buf, "")) {
+         yield(buf);
+         buf = try reader.readLine() 
+       }
+    }
+    while  
+}
+
 pub fn main() !void {
 
     var gp = std.heap.GeneralPurposeAllocator(.{ }){};
@@ -11,14 +25,14 @@ pub fn main() !void {
     
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
-    var buf: [1024]u8 = undefined;
+    var buf: [1024] u8 = undefined;
 
     var current: usize = 0;
     
     var list = std.ArrayList(usize).init(allocator);
     defer list.deinit();
 
-    while( try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+    while( try iter.next() ) |line| {
       if (std.mem.eql(u8,line,"")) {
          try list.append(current);
          current = 0;
---
+++ 2022/7/reference.hs
@@ -0,0 +1,25 @@
+import Data.List
+import qualified Data.Map as M
+
+main = do
+  input <- lines <$> getContents
+  -- print input
+  -- print $ process input ["/"] M.empty
+  let s = process input ["/"] M.empty
+  let total_current = s M.! []
+  print $ sum $ M.elems $ M.filter (<= 100000) s
+  print $ head $ sort $ M.elems $ M.filter (\s -> 70000000 - total_current + s > 30000000) s
+
+type Path = [String]
+
+process :: [String] -> Path -> M.Map Path Int -> M.Map Path Int
+process [] _ x = x
+process (c:cs) p@(_:ps) x = case c of
+  "$ cd .." -> process cs ps x
+  ('$':' ':'c':'d':' ':cwd) -> process cs (cwd:p) x
+  "$ ls" -> 
+    let (dirs,cmds) = break (\s -> head s == '$') cs 
+        size = sum $ map read $ filter (/="dir") $ map (head .words) dirs 
+        x' = foldl' (\z path -> M.insertWith (+) path size z) x (tails p)
+    in process cmds p x'
+  
